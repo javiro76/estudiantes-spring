79671f7de2153cf49f4e3f7ab59b966e63f0d949
javier <javier.rodriguez@dev-codes.io>
2025-04-06 19:55:40 -0500

    ADD: configuration in messages of the spring

--- src/main/java/jr/estudiantes/EstudiantesApplication.java
+++ src/main/java/jr/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,33 @@
 package jr.estudiantes;
 
+import jr.estudiantes.servicio.EstudianteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
+		logger.info("Iniciando la aplicación...");
+		//levanta fabrica de spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Aplicacion finalizada!");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl + "Ejecutando metododo run de spring..." + nl);
+	}
 }
